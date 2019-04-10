c37549110086f4221221409fa75d195732f1f57a
hparkae <hparkae@csl4wk08.win2k.cs.ust.hk>
2019-04-10 15:22:27 +0800

    I hope it works this time

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,7 +5,7 @@ public class Library {
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
-			System.out.println("When there is a fire, commit and push.");
+			System.out.println("Does it work now.");
 	}
     public boolean someLibraryMethod() {
         return true;
