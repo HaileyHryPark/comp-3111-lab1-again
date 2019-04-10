964f5561c8949b10e1a58958236ff4da7ce662a6
Hailey Park <16parkha@gmail.com>
2019-04-10 14:42:12 +0800

    Park

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -6,7 +6,7 @@ public class Library {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
 			System.out.println("I dont understand either");
-			System.out.println("I dont understand anything");
+			System.out.println("I am starting to understand");
 	}
     public boolean someLibraryMethod() {
         return true;
